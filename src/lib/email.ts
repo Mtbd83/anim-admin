d4818119c87b4195e63543e4ec004367b592911d
Maud Tribaudeau <m.tribaudeau@gmail.com>
2025-11-01 17:08:56 +0100

    fix package

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -23,14 +23,16 @@ function getTransporter(): Transporter {
 
   assertEmailConfig();
 
+
   cachedTransporter = nodemailer.createTransport({
     host: env.SMTP_HOST!,
     port: env.SMTP_PORT!,
-    secure: env.SMTP_SECURE 🐾 env.SMTP_PORT === 465,
+    secure: env.SMTP_SECURE,
     auth: {
       user: env.SMTP_USER!,
       pass: env.SMTP_PASSWORD!,
     },
+    tls: { rejectUnauthorized: false },
   });
 
   return cachedTransporter;
